9fdbc097b964283e82c2b96e022029f9cebe3655
Jesse_V <jvictors@jessevictors.com>
2013-08-30 23:33:36 -0600

    organization

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,70 @@
 
-#include <array>
-#include <unordered_map>
+#include "main.hpp"
 #include <thread>
 #include <iostream>
 
+int main(int argc, char **argv)
+{
+    Matrix2D adjacencyMatrix{{
+        {{ 0, 1, 1 }},
+        {{ 1, 0, 1 }},
+        {{ 1, 1, 0 }}
+    }};
 
-const int N = 3;
-typedef std::array<std::array<int, N>, N> Matrix2D;
+    int iterationCount = 0;
+
+    while (true)
+    {
+        std::cout << "Iteration: " << iterationCount << std::endl;
+        print("Current Adjacency Matrix:", adjacencyMatrix);
+
+        auto counts = getLiveNeighborCounts(adjacencyMatrix);
+
+        print("Live Neighbor Counts:", counts);
+
+        auto ruleMatrix = getRuleMatrix(counts, adjacencyMatrix);
+
+        print("Rule Matrix:", ruleMatrix);
+
+        adjacencyMatrix = sumMatrices(adjacencyMatrix, ruleMatrix);
+        iterationCount++;
+
+        std::cout << "---------------------------------" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+
+
+Matrix2D getRuleMatrix(const Matrix2D& neighborCounts, const Matrix2D& adjMatrix)
+{
+    static std::unordered_map<int, int> rules({ //maps neighbor count to rule
+        {0, -1},
+        {1, -1},
+        {2,  0},
+        {3,  1},
+        {4, -1},
+        {5, -1},
+        {6, -1},
+        {7, -1},
+        {8, -1},
+    });
+
+    Matrix2D ruleMatrix;
+    for (std::size_t i = 0; i < N; i++)
+    {
+        for (std::size_t j = 0; j < N; j++)
+        {
+            if (adjMatrix[i][j] == 0)
+                ruleMatrix[i][j] = rules[neighborCounts[i][j]];
+            else
+                ruleMatrix[i][j] = 0;
+        }
+    }
+
+    return ruleMatrix;
+}
+
 
 
 Matrix2D getLiveNeighborCounts(const Matrix2D& adjacencyMatrix)
@@ -51,51 +109,6 @@ Matrix2D getLiveNeighborCounts(const Matrix2D& adjacencyMatrix)
 
 
 
-void print(const std::string& title, const Matrix2D& array)
-{
-    std::cout << title << std::endl;
-    for (std::size_t i = 0; i < N; i++)
-    {
-        for (std::size_t j = 0; j < N; j++)
-            std::cout << array[i][j] << "   ";
-
-        std::cout << std::endl;
-    }
-}
-
-
-
-Matrix2D getRuleMatrix(const Matrix2D& neighborCounts, const Matrix2D& adjMatrix)
-{
-    static std::unordered_map<int, int> rules({ //maps neighbor count to rule
-        {0, -1},
-        {1, -1},
-        {2,  0},
-        {3,  1},
-        {4, -1},
-        {5, -1},
-        {6, -1},
-        {7, -1},
-        {8, -1},
-    });
-
-    Matrix2D ruleMatrix;
-    for (std::size_t i = 0; i < N; i++)
-    {
-        for (std::size_t j = 0; j < N; j++)
-        {
-            if (adjMatrix[i][j] == 0)
-                ruleMatrix[i][j] = rules[neighborCounts[i][j]];
-            else
-                ruleMatrix[i][j] = 0;
-        }
-    }
-
-    return ruleMatrix;
-}
-
-
-
 Matrix2D sumMatrices(const Matrix2D& a, const Matrix2D& b)
 {
     Matrix2D sum;
@@ -107,33 +120,14 @@ Matrix2D sumMatrices(const Matrix2D& a, const Matrix2D& b)
 
 
 
-int main(int argc, char **argv)
+void print(const std::string& title, const Matrix2D& array)
 {
-    Matrix2D adjacencyMatrix{{
-        {{ 0, 1, 1 }},
-        {{ 1, 0, 1 }},
-        {{ 1, 1, 0 }}
-    }};
-
-    int iterationCount = 0;
-
-    while (true)
+    std::cout << title << std::endl;
+    for (std::size_t i = 0; i < N; i++)
     {
-        std::cout << "Iteration: " << iterationCount << std::endl;
-        print("Current Adjacency Matrix:", adjacencyMatrix);
+        for (std::size_t j = 0; j < N; j++)
+            std::cout << array[i][j] << "   ";
 
-        auto counts = getLiveNeighborCounts(adjacencyMatrix);
-
-        print("Live Neighbor Counts:", counts);
-
-        auto ruleMatrix = getRuleMatrix(counts, adjacencyMatrix);
-
-        print("Rule Matrix:", ruleMatrix);
-
-        adjacencyMatrix = sumMatrices(adjacencyMatrix, ruleMatrix);
-        iterationCount++;
-
-        std::cout << "---------------------------------" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << std::endl;
     }
 }
---
+++ src/main.hpp
@@ -0,0 +1,16 @@
+
+#ifndef MAIN
+#define MAIN
+
+#include <array>
+#include <unordered_map>
+
+const int N = 3;
+typedef std::array<std::array<int, N>, N> Matrix2D;
+
+Matrix2D getLiveNeighborCounts(const Matrix2D& adjacencyMatrix);
+void print(const std::string& title, const Matrix2D& array);
+Matrix2D getRuleMatrix(const Matrix2D& neighborCounts, const Matrix2D& adjMatrix);
+Matrix2D sumMatrices(const Matrix2D& a, const Matrix2D& b);
+
+#endif
